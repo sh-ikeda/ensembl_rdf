9473e01d1c893c0d8001f39800c054bb7dfa2620
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2023-05-18 17:50:59 +0900

    initial commit

---
+++ parse_json.py
@@ -0,0 +1,15 @@
+import ijson
+import sys
+
+
+def main():
+    input_json_file = sys.argv[1]
+
+    with open(input_json_file, "r") as input_json:
+        ijson_generator = ijson.items(input_json, "genes.item")
+        for gene in ijson_generator:
+            print(gene["id"], gene.keys())
+
+
+if __name__ == "__main__":
+    main()
