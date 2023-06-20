b4442df9508267cd49e0e6e79b0fbb06efd89a33
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2023-06-20 16:48:02 +0900

    change output

--- convert.sh
+++ convert.sh
@@ -7,14 +7,14 @@ for d in $( ls . | grep "_core_" ); do
         continue
     fi
     cd $d
-    echo $d
+    echo $d 1>&2
     python3 $SCRIPT_DIR/rdf_converter_ensembl_db.py $SCRIPT_DIR/dbinfo.json
 
     #echo "Validating turtle files..."
     for f in gene transcript translation exon exon_transcript xref ; do
         rapper -i turtle -o turtle $f.ttl > $f.rapper.ttl
-        /bin/rm $f.ttl
-        gzip -f $f.rapper.ttl
+        mv $f.rapper.ttl $f.ttl
+        gzip -f $f.ttl
     done
     cd ..
 done
