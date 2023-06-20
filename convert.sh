5e36850c936a679cf5a4ab7bdefa190d8ec31549
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2023-06-20 15:57:37 +0900

    add script

---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+set -euo pipefail
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+for d in $( ls . | grep "_core_" ); do
+    if [ ! -d $d ]; then
+        continue
+    fi
+    cd $d
+    echo $d
+    python3 $SCRIPT_DIR/rdf_converter_ensembl_db.py $SCRIPT_DIR/dbinfo.json
+
+    #echo "Validating turtle files..."
+    for f in gene transcript translation exon exon_transcript xref ; do
+        rapper -i turtle -o turtle $f.ttl > $f.rapper.ttl
+        /bin/rm $f.ttl
+        gzip -f $f.rapper.ttl
+    done
+    cd ..
+done
